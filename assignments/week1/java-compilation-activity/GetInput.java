93d61f1b7b737449bfc63e6b5a48c972691d37d5
Calvin Post <calvinhenry826@revature.net>
2022-06-15 07:05:32 -0400

    Added methods to GetInput

--- assignments/week1/java-compilation-activity/GetInput.java
+++ assignments/week1/java-compilation-activity/GetInput.java
@@ -2,27 +2,57 @@ public class GetInput {
 
 	public static void main(String[] args) {
 
+		int n = 0;
+
+		if(args.length > 0) {
+			n = Integer.parseInt(args[0]);
+		} else {
+			message();
+		}
+
 		switch (args.length) {
 
 		//Get random number
 		case 1:
-		int rand = (int)(Math.random() * Integer.parseInt(args[0])) + 1;
-		System.out.println(rand);
+		printRandom(n);
 		break;
 
 		//Print a string x times
 		case 2:
-		int x = Integer.parseInt(args[0]);
-		for (int i=0; i<x; i++) {
-			System.out.println(args[1]);
-		}
+		printString(n, args[1]);
 		break;
 
 		//Default case
 		default:
+		message();
 		break;
 
 		}
+
+	}
+
+	public static void printRandom(int x) {
+	
+		int i = (int)(Math.random()*x) + 1;
+		System.out.println(i);
+
+	}
+
+	public static void printString(int x, String s) {
+
+		for(int i=0; i<x; i++) {
+
+			System.out.println(s);
+		}
+
+	}
+
+	public static void message() {
+	
+		System.out.println("Invalid input, use the following formats:");
+		System.out.println("GetInput 10");
+		System.out.println("GetInput 8 hello");
+		System.out.println("GetInput 5 \"Hello World!\"");
 
 	}
 
