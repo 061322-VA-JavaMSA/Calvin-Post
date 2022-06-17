b0bd5a1ebc7395fdee6bc319fb9d0a02f0204fb0
Calvin Post <calvinhenry826@revature.net>
2022-06-17 17:21:18 -0400

    Updated testing

--- assignments/week1/testing-demo/src/test/java/com/revature/CalculatorTest.java
+++ assignments/week1/testing-demo/src/test/java/com/revature/CalculatorTest.java
@@ -32,99 +32,48 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void add() {
-		System.out.println("Testing add");
-		for (int i = 0; i < 5; i++) {
-			int a = (int) (Math.random() * 51);
-			int b = (int) (Math.random() * 51);
-			int expected = a + b;
-			int actual = sut.add(a, b);
-			System.out.println("input: (" + a + ", " + b + ") | expected: " + expected + " | actual: " + actual);			assertEquals(expected, actual);
-		}
+	public void addFourFive() {
+		int expected = 9;
+		int actual = sut.add(4, 5);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void subtract() {
-		System.out.println("Testing subtract");
-		for (int i = 0; i < 5; i++) {
-			int a = (int) (Math.random() * 51);
-			int b = (int) (Math.random() * 51);
-			int expected = a - b;
-			int actual = sut.subtract(a, b);
-			System.out.println("input: (" + a + ", " + b + ") | expected: " + expected + " | actual: " + actual);			assertEquals(expected, actual);
-		}
+	public void subtractFourFive() {
+		int expected = -1;
+		int actual = sut.subtract(4, 5);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void multiply() {
-		System.out.println("Testing multiply");
-		for (int i = 0; i < 5; i++) {
-			int a = (int) (Math.random() * 51);
-			int b = (int) (Math.random() * 51);
-			int expected = a * b;
-			int actual = sut.multiply(a, b);
-			System.out.println("input: (" + a + ", " + b + ") | expected: " + expected + " | actual: " + actual);
-			assertEquals(expected, actual);
-		}
+	public void multiplyFourFive() {
+		int expected = 20;
+		int actual = sut.multiply(4, 5);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void divide() {
-		System.out.println("Testing divide");
-		for (int i = 0; i < 5; i++) {
-			int a = (int) (Math.random() * 50 + 1);
-			int b = (int) (Math.random() * 50 + 1);
-			int expected = a / b;
-			int actual = sut.divide(a, b);
-			System.out.println("input: (" + a + ", " + b + ") | expected: " + expected + " | actual: " + actual);
-			assertEquals(expected, actual);
-		}
+	public void divideSixTwo() {
+		int expected = 3;
+		int actual = sut.divide(6, 2);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void addEquals13() {
-		System.out.println("Testing when add equals 13");
-		for (int i = 0; i <= 13; i++) {
-			for (int j = 0; j <= 13; j++) {
-				if (i + j == 13) {
-					int a = i, b = j;
-					System.out.println("input: (" + a + ", " + b + ")");
-					assertThrows(NumberThirteenException.class, () -> sut.add(a, b));
-				}
-			}
-		}
+	public void addSixSeven() {
+		assertThrows(NumberThirteenException.class, () -> sut.add(6, 7));
 	}
 
 	@Test
-	public void divideByZero() {
-		System.out.println("Testing dividing by 0");
-		for (int i = 0; i < 5; i++) {
-			int a = (int) (Math.random() * 51);
-			int b = 0;
-			System.out.println("input: (" + a + ", " + b + ")");
-			assertThrows(DivideByZeroException.class, () -> sut.divide(a, b));
-		}
+	public void divideFiveByZero() {
+		assertThrows(DivideByZeroException.class, () -> sut.divide(5, 0));
 	}
 
 	@Test
-	public void sum() {
-		System.out.println("Testing sum");
-		for (int i = 0; i < 5; i++) {
-			int[] arr = new int[(int) (Math.random() * 10 + 1)];
-			int expected = 0;
-			for (int j = 0; j < arr.length; j++) {
-				int a = (int) (Math.random() * 51);
-				arr[j] = a;
-				expected += a;
-			}
-			int actual = sut.sum(arr);
-
-			System.out.print("input: [");
-			for (int j = 0; j < arr.length - 1; j++) {
-				System.out.print(j + ", ");
-			}
-			System.out.println(arr[arr.length - 1] + "] | expected: " + expected + " | actual: " + actual);
-			assertEquals(expected, actual);
-		}
+	public void sumFourFiveSix() {
+		int[] arr = {4, 5, 6};
+		int expected = 15;
+		int actual = sut.sum(arr);
+		assertEquals(expected, actual);
 	}
 }
